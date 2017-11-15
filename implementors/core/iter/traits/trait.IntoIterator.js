8bd6e963ae2d6bf01062b6371115f4b0153d278c
Francesco Cogno <francesco.cogno@outlook.com>
2017-11-15 15:19:32 +0100

    0.6.0

---
+++ implementors/core/iter/traits/trait.IntoIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["azure_sdk_for_rust"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"azure_sdk_for_rust/azure/cosmos/partition_key/struct.PartitionKey.html\" title=\"struct azure_sdk_for_rust::azure::cosmos::partition_key::PartitionKey\">PartitionKey</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
